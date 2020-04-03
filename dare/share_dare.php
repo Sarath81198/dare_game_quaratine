c57c1e0b3ea5e1fc8a322c05b297b0f8ca1793f2
Sarath Dev <sarathsekaran2007@gmail.com>
2020-04-04 01:48:56 +0530

    Added WhatsApp share

--- dare/share_dare.php
+++ dare/share_dare.php
@@ -100,6 +100,10 @@ if ($dare_users_collection->insertOne($set_dare)) {
     echo 0;
 }
 ?>
+<meta property="og:title" content="Do the Dare!" />
+<meta property="og:url" content="http://<?php echo $base_URL; ?>/dare/dare.php?id=<?php echo $dare_id_given ?>" />
+<meta property="og:description" content="You can set your dare and send it to your special ones">
+
 <h1>Hey <?php echo $dare_from; ?>, share this to <?php echo $dare_to; ?></h1>
 http://<?php echo $base_URL; ?>/dare/dare.php?id=<?php echo $dare_id_given ?><br>
-<a href="whatsapp://send?text=<?php echo $dare_to; ?>, here's the dare for you" data-action="http://<?php echo $base_URL; ?>/dare/dare.php?id=<?php echo $dare_id_given ?>">Share via Whatsapp</a>
+<a href="whatsapp://send?text=<?php echo $dare_to; ?>, here's the dare for you. http://<?php echo $base_URL; ?>/dare/dare.php?id=<?php echo $dare_id_given ?>" data-action="http://<?php echo $base_URL; ?>/dare/dare.php?id=<?php echo $dare_id_given ?>">Share via Whatsapp</a>
