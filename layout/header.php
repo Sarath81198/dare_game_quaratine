4c8bf8ca307962f1f216ff9466063aa2c7a18795
Sarath Dev <sarathsekaran2007@gmail.com>
2020-04-05 16:36:56 +0530

    Website name font updated

--- layout/header.php
+++ layout/header.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Google Font -Aladin -->
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
     <!-- Google Chrome for Android -->
     <meta name="theme-color" content="#343a40">
     <!-- WhatsApp -->
