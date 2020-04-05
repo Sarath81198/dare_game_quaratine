40cc7eba67df81c617b5c1125c09acfdc8deb361
Sarath Dev <sarathsekaran2007@gmail.com>
2020-04-05 16:32:38 +0530

    Website name font updated

--- layout/header.php
+++ layout/header.php
@@ -23,7 +23,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom: 40px">
-        <a class="navbar-brand" href="/"><b>GRATIFY</b></a>
+        <a class="navbar-brand" href="/" style="font-family: 'Aladin', cursive;"><b>Gratify</b></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
