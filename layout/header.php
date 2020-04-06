f53b31e82bf08da48ad109179ff5a0ae5ee12bbb
Sarath Dev <sarathsekaran2007@gmail.com>
2020-04-06 13:36:27 +0530

    Nav bar updated

--- layout/header.php
+++ layout/header.php
@@ -38,14 +38,25 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom: 40px">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom: 80px">
         <a class="navbar-brand" href="/" style="font-family: 'Aladin', cursive;"><b>Gratify</b></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link" href="/">Home</a>
-            </div>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dare
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../dare/set_dare.php">Set Dare</a>
+                    </div>
+                </li>
+            </ul>
         </div>
     </nav>
